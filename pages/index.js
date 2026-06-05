3b43b44e0caf7b9622036415929d86d019753668
jai-tech-stack <jaikumard01@gmail.com>
2026-06-05 08:07:55 +0530

    Remove whitewolf.api.js - eliminates Turnstile error (we use custom contact form)

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,6 @@ export default function Home({ bodyHtml, inlineStyles }) {
       "/assets/ScrollTrigger.min.js",
       "/assets/lenis.js",
       "/assets/whitewolf-core.js",
-      "/assets/whitewolf.api.js",
       "/assets/whitewolf-modules.js",
       "/assets/whitewolf-ui.js",
       "/assets/whitewolf.fonts.js",
